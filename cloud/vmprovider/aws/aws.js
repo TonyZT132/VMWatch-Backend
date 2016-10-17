0f04f203604040e99b7b2bf96e9e9daaa002a9f7
TonyZT132 <tuo.zhang@mail.utoronto.ca>
2016-10-17 16:32:12 -0400

    change period time to 10min

--- cloud/vmprovider/aws/aws.js
+++ cloud/vmprovider/aws/aws.js
@@ -25,7 +25,7 @@ module.exports = {
             MetricName : "CPUUtilization",
             StartTime  : startDate,
             EndTime    : new Date(),
-            Period     : 300
+            Period     : 600
         };
 
         cloudwatch.getMetricStatistics(params, function(cloudWatchErr, response) {
