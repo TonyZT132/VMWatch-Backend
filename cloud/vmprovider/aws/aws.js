9df0a5f0932f5e80e47b53e2bb7bc7b7d2bba537
Tuo Zhang <tuo.zhang@mail.utoronto.ca>
2016-11-27 22:48:55 -0500

    added networkout matrics

--- cloud/vmprovider/aws/aws.js
+++ cloud/vmprovider/aws/aws.js
@@ -21,6 +21,57 @@ module.exports = {
                 }],
                 Unit: "Percent",
                 Statistics: ["Average", "Maximum", "Minimum"],
+                MetricName: "CPUUtilization",
+                StartTime: startDate,
+                EndTime: new Date(),
+                Period: 300
+            };
+
+            cloudwatch.getMetricStatistics(params, function(cloudWatchErr, response) {
+                var data = null;
+                var error = null;
+
+                if (cloudWatchErr) {
+                    winston.error("Error: " + err);
+                    callback(error, data);
+                } else {
+                    try {
+                        winston.info("Sucessfully get data from CloudWatch");
+                        data = response;
+                    } catch (exception) {
+                        error = exception;
+                    } finally {
+                        winston.info("Returning data");
+                        callback(error, data);
+                    }
+                }
+            });
+        } catch (err) {
+            winston.info("Error catched from AWS SDK");
+            callback(err.message, null);
+        }
+    }
+
+    getNetworkOut: function(accessID, accessKey, instanceID, instanceRegion, callback) {
+        winston.info('Getting metrics from AWS EC2');
+        var startDate = getStartTime(new Date(), 20);
+        try {
+            var AWS = require('aws-sdk');
+            AWS.config.update({
+                accessKeyId: accessID,
+                secretAccessKey: accessKey,
+                region: instanceRegion
+            });
+
+            var cloudwatch = new AWS.CloudWatch();
+            var params = {
+                Namespace: "AWS/EC2",
+                Dimensions: [{
+                    Name: "InstanceId",
+                    Value: instanceID
+                }],
+                Unit: "Bytes",
+                Statistics: ["Average", "Maximum", "Minimum"],
                 MetricName: "NetworkOut",
                 StartTime: startDate,
                 EndTime: new Date(),
