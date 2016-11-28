995e084f8c5a34d7c65e6b8fed42e23ebf261df3
Tuo Zhang <tuo.zhang@mail.utoronto.ca>
2016-11-27 23:14:34 -0500

    update aws module

--- cloud/vmprovider/aws/aws.js
+++ cloud/vmprovider/aws/aws.js
@@ -1,9 +1,9 @@
 var winston = require('winston');
 
 module.exports = {
-    getCPUUtilization: function(accessID, accessKey, instanceID, instanceRegion, callback) {
+    getMonitoringData: function(accessID, accessKey, instanceID, instanceRegion, metrics, range, callback) {
         winston.info('Getting metrics from AWS EC2');
-        var startDate = getStartTime(new Date(), 20);
+        var startDate = getStartTime(new Date(), range);
         try {
             var AWS = require('aws-sdk');
             AWS.config.update({
@@ -13,123 +13,7 @@ module.exports = {
             });
 
             var cloudwatch = new AWS.CloudWatch();
-            // var params = {
-            //     Namespace: "AWS/EC2",
-            //     Dimensions: [{
-            //         Name: "InstanceId",
-            //         Value: instanceID
-            //     }],
-            //     Unit: "Percent",
-            //     Statistics: ["Average", "Maximum", "Minimum"],
-            //     MetricName: "CPUUtilization",
-            //     StartTime: startDate,
-            //     EndTime: new Date(),
-            //     Period: 300
-            // };
-
-            var params = returnCPUUtilizationParams(instanceID, startDate);
-
-            cloudwatch.getMetricStatistics(params, function(cloudWatchErr, response) {
-                var data = null; var error = null;
-
-                if (cloudWatchErr) {
-                    winston.error("Error: " + err);
-                    callback(error, data);
-                } else {
-                    try {
-                        winston.info("Sucessfully get data from CloudWatch");
-                        data = response;
-                    } catch (exception) {
-                        error = exception;
-                    } finally {
-                        winston.info("Returning data");
-                        callback(error, data);
-                    }
-                }
-            });
-        } catch (err) {
-            winston.info("Error catched from AWS SDK");
-            callback(err.message, null);
-        }
-    },
-    getNetworkIn: function(accessID, accessKey, instanceID, instanceRegion, callback) {
-        winston.info('Getting metrics from AWS EC2');
-        var d = new Date();
-        startDate.setHours(startDate.getHours() - 1);
-
-        try {
-            var AWS = require('aws-sdk');
-            AWS.config.update({
-                accessKeyId: accessID,
-                secretAccessKey: accessKey,
-                region: instanceRegion
-            });
-
-            var cloudwatch = new AWS.CloudWatch();
-            var params = {
-                Namespace: "AWS/EC2",
-                Dimensions: [{
-                    Name: "InstanceId",
-                    Value: instanceID
-                }],
-                Unit: "Bytes",
-                Statistics: ["Average", "Maximum", "Minimum"],
-                MetricName: "NetworkIn",
-                StartTime: startDate,
-                EndTime: new Date(),
-                Period: 300
-            };
-
-            cloudwatch.getMetricStatistics(params, function(cloudWatchErr, response) {
-                var data = null; var error = null;
-
-                if (cloudWatchErr) {
-                    winston.error("Error: " + err);
-                    callback(error, data);
-                } else {
-                    try {
-                        winston.info("Sucessfully get data from CloudWatch");
-                        data = response;
-                    } catch (exception) {
-                        error = exception;
-                    } finally {
-                        winston.info("Returning data");
-                        callback(error, data);
-                    }
-                }
-            });
-        } catch (err) {
-            winston.info("Error catched from AWS SDK");
-            callback(err.message, null);
-        }
-    },
-    getNetworkOut: function(accessID, accessKey, instanceID, instanceRegion, callback) {
-        winston.info('Getting metrics from AWS EC2');
-        var d = new Date();
-        startDate.setHours(startDate.getHours() - 1);
-
-        try {
-            var AWS = require('aws-sdk');
-            AWS.config.update({
-                accessKeyId: accessID,
-                secretAccessKey: accessKey,
-                region: instanceRegion
-            });
-
-            var cloudwatch = new AWS.CloudWatch();
-            var params = {
-                Namespace: "AWS/EC2",
-                Dimensions: [{
-                    Name: "InstanceId",
-                    Value: instanceID
-                }],
-                Unit: "Bytes",
-                Statistics: ["Average", "Maximum", "Minimum"],
-                MetricName: "NetworkOut",
-                StartTime: startDate,
-                EndTime: new Date(),
-                Period: 300
-            };
+            var params = getParams(instanceID, startDate, metrics);
 
             cloudwatch.getMetricStatistics(params, function(cloudWatchErr, response) {
                 var data = null;
@@ -158,10 +42,23 @@ module.exports = {
 };
 
 function getStartTime(date, minutes) {
-    return new Date(date.getTime() - minutes*60000);
+    return new Date(date.getTime() - minutes * 60000);
 }
 
-function returnCPUUtilizationParams(instanceID, startDate){
+function getParams(instanceID, startDate, metrics) {
+    switch (metrics) {
+        case "CPUUtilization":
+            return cpuUtilizationParams(instanceID, startDate);
+        case "NetworkIn":
+            return networkInParams(instanceID, startDate);
+        case "NetworkOut":
+            return networkOutParams(instanceID, startDate);
+        default:
+            return null;
+    }
+}
+
+function cpuUtilizationParams(instanceID, startDate) {
     return {
         Namespace: "AWS/EC2",
         Dimensions: [{
@@ -171,6 +68,38 @@ function returnCPUUtilizationParams(instanceID, startDate){
         Unit: "Percent",
         Statistics: ["Average", "Maximum", "Minimum"],
         MetricName: "CPUUtilization",
+        StartTime: startDate,
+        EndTime: new Date(),
+        Period: 300
+    };
+}
+
+function networkInParams(instanceID, startDate) {
+    return {
+        Namespace: "AWS/EC2",
+        Dimensions: [{
+            Name: "InstanceId",
+            Value: instanceID
+        }],
+        Unit: "Bytes",
+        Statistics: ["Average", "Maximum", "Minimum"],
+        MetricName: "NetworkIn",
+        StartTime: startDate,
+        EndTime: new Date(),
+        Period: 300
+    };
+}
+
+function networkOutParams(instanceID, startDate) {
+    return {
+        Namespace: "AWS/EC2",
+        Dimensions: [{
+            Name: "InstanceId",
+            Value: instanceID
+        }],
+        Unit: "Bytes",
+        Statistics: ["Average", "Maximum", "Minimum"],
+        MetricName: "NetworkOut",
         StartTime: startDate,
         EndTime: new Date(),
         Period: 300
