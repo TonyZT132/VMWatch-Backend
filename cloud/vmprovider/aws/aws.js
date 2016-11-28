5f64f6ab43d1d45760edaf9ec052506a071112cf
Tuo Zhang <tuo.zhang@mail.utoronto.ca>
2016-11-27 22:43:37 -0500

    test matrics

--- cloud/vmprovider/aws/aws.js
+++ cloud/vmprovider/aws/aws.js
@@ -21,7 +21,7 @@ module.exports = {
                 }],
                 Unit: "Percent",
                 Statistics: ["Average", "Maximum", "Minimum"],
-                MetricName: "CPUUtilization | NetworkOut",
+                MetricName: "NetworkOut",
                 StartTime: startDate,
                 EndTime: new Date(),
                 Period: 300
