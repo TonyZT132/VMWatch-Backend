82bff9b80270b187d9bd3eee24220789ba01785d
Tuo Zhang <tuo.zhang@mail.utoronto.ca>
2017-01-07 14:58:23 -0500

    switch back to original call method

--- cloud/main.js
+++ cloud/main.js
@@ -170,53 +170,22 @@ Parse.Cloud.define("serviceRequest", function(request, response) {
     }
 });
 
+
 /*Interface for Monitoring EC2 Data*/
 Parse.Cloud.define("ec2Watch", function(request, response) {
     var accessID = request.params.accessid;
     var accessKey = request.params.accesskey;
     var instanceID = request.params.instanceid;
     var region = request.params.region;
-
-    var config = require('./config');
-    var cat = config.METRICS_EC2;
-
-    var result = [];
+    var metrics = request.params.metrics;
+    var range = request.params.range;
 
     winston.info("Start EC2 Watch");
-    ec2Watch.getMonitoringData(accessID, accessKey, instanceID, region, cat.metrics[0].name, cat.metrics[0].range, function(error, data) {
+    ec2Watch.getMonitoringData(accessID, accessKey, instanceID, region, metrics, range, function(error, data) {
         if (error) {
             response.error(error);
         } else {
-            result.push(data);
-            ec2Watch.getMonitoringData(accessID, accessKey, instanceID, region, cat.metrics[1].name, cat.metrics[1].range, function(error, data) {
-                if (error) {
-                    response.error(error);
-                } else {
-                    result.push(data);
-                    ec2Watch.getMonitoringData(accessID, accessKey, instanceID, region, cat.metrics[2].name, cat.metrics[2].range, function(error, data) {
-                        if (error) {
-                            response.error(error);
-                        } else {
-                            result.push(data);
-                            ec2Watch.getMonitoringData(accessID, accessKey, instanceID, region, cat.metrics[3].name, cat.metrics[3].range, function(error, data) {
-                                if (error) {
-                                    response.error(error);
-                                } else {
-                                    result.push(data);
-                                    ec2Watch.getMonitoringData(accessID, accessKey, instanceID, region, cat.metrics[4].name, cat.metrics[4].range, function(error, data) {
-                                        if (error) {
-                                            response.error(error);
-                                        } else {
-                                            result.push(data);
-                                            response.success(JSON.stringify(result));
-                                        }
-                                    });
-                                }
-                            });
-                        }
-                    });
-                }
-            });
+            response.success(data);
         }
     });
 });
