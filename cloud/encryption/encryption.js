d51e63edc3379341d125ed583645d20a3449afe4
Tuo Zhang <tuo.zhang@mail.utoronto.ca>
2017-02-21 22:12:59 -0500

    finalize encryption model

--- cloud/encryption/encryption.js
+++ cloud/encryption/encryption.js
@@ -1,24 +1,6 @@
-// var CryptoJS = require("crypto-js");
 var crypto = require('crypto');
-var winston = require('winston');
 
 module.exports = {
-    // requestMasterKey: function() {
-    //     winston.info("Requesting master key");
-    //
-    //     var passphrase = process.env.PASSPHRASE;
-    //     // random on a per-user basis
-    //     var salt = crypto.randomBytes(32);
-    //
-    //     // keep it well under 10,000
-    //     var iterations = 267;
-    //
-    //     var keyByteLength = 32; // desired length for an AES key
-    //
-    //     crypto.pbkdf2(passphrase, salt, iterations, keyByteLength, 'sha256', function(err, bytes) {
-    //         console.log(bytes.toString('hex'));
-    //     });
-    // },
     encrypt: function(text) {
         var cipher = crypto.createCipher(process.env.DB_ENCRYPTION_ALGORITHM, process.env.DB_ENCRYPTION_MASTER_KEY)
         var crypted = cipher.update(text, 'utf8', 'hex')
@@ -32,6 +14,3 @@ module.exports = {
         return dec;
     }
 };
-// var hw = encrypt("hello world")
-// // outputs hello world
-// console.log(decrypt(hw));
