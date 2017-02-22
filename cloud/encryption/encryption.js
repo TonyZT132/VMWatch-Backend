3bead41a6895218574faf722013d758cc391e27a
Tuo Zhang <tuo.zhang@mail.utoronto.ca>
2017-02-22 12:40:01 -0500

    added key generator

--- cloud/encryption/encryption.js
+++ cloud/encryption/encryption.js
@@ -1,6 +1,20 @@
 var crypto = require('crypto');
 
 module.exports = {
+    generateClientKey: function() {
+        var passphrase = process.env.PASSPHRASE;
+        var salt = crypto.randomBytes(32);
+        var iterations = 267;
+        var keyByteLength = 32;
+
+        crypto.pbkdf2(passphrase, salt, iterations, keyByteLength, 'sha256', function(err, bytes) {
+            if (err == null) {
+                return bytes.toString('hex');
+            } else {
+                return null;
+            }
+        });
+    },
     encrypt: function(text) {
         var cipher = crypto.createCipher(process.env.DB_ENCRYPTION_ALGORITHM, process.env.DB_ENCRYPTION_MASTER_KEY)
         var crypted = cipher.update(text, 'utf8', 'hex')
