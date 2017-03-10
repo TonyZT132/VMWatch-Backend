75d989a85326c629f8685b39303ba2eb27fdab74
Yuxuan Zhang <edwinyuxuan.zhang@mail.utoronto.ca>
2017-03-09 21:38:17 -0500

    change to aws/google/azure only

--- cloud/config.js
+++ cloud/config.js
@@ -4,25 +4,15 @@ var SERVICE_CONFIG = {
         "name": "aws",
         "icon":"aws-logo",
         "avaliable": true
-    }, {
+    },{
         "id": 1,
-        "name": "azure",
-        "icon":"azure-logo",
-        "avaliable": false
-    }, {
-        "id": 2,
         "name": "google",
         "icon":"google-logo",
         "avaliable": true
-    }, {
-        "id": 3,
-        "name": "heroku",
-        "icon":"heroku-logo",
-        "avaliable": false
-    }, {
-        "id": 4,
-        "name": "vmware",
-        "icon":"vmware-logo",
+    },{
+        "id": 2,
+        "name": "azure",
+        "icon":"azure-logo",
         "avaliable": false
     }],
 };
